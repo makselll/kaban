cc60ca5b1b5fe0e524c63f1e757ec45d8a2f6d99
Максим Шелгунов <maksimselgunov@Maksims-MacBook-Pro.local>
2025-03-06 11:01:45 +0700

    removed phantom a/b/c dir

--- chapter_5/basic_master_slave_replication/blog/postgres/00_init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
-SELECT pg_create_physical_replication_slot('replication_slot_1');
-SELECT pg_create_physical_replication_slot('replication_slot_2');
